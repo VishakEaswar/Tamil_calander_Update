68647b7ab3a4112e12a067bb1cbb410ae78e249a
Vishak Easwer <vishakeaswer@gmail.com>
2026-01-04 15:27:00 +0530

    commit-6

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: "/Tamil_calander_Update/",
-  server: {
-    host: true, // or '0.0.0.0'
-    port: 5174,
+  base: "/Tamil_calander_Update/",
+  build: {
+    outDir: "docs",
+    emptyOutDir: true
   },
+  server: {
+    host: true,
+    port: 5174
+  }
 })
